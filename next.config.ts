447217b8c23f88f0e16892044f26c31803f0aee6
gabrielgad <valdnato@gmail.com>
2025-09-13 19:51:48 -0300

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,23 @@
+
+import {NextConfig} from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
+ 
+const nextConfig: NextConfig = {
+    images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+ 
+const withNextIntl = createNextIntlPlugin();
+export default withNextIntl(nextConfig);
